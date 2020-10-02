1ce1b0dbb80a58b1b71dc8822150360344c244d9
starprince7 <66694387+starprince7@users.noreply.github.com>
2020-10-03 00:42:32 +0100

    Delete actionTypes.js

--- src/redux/actionTypes.js
+++
@@ -1,7 +0,0 @@
-export const GET_USERS = 'GET_USERS'
-export const ADD_USER = 'ADD_USER'
-export const DELETE_USER = 'DELETE_ITEMS'
-export const ADD_COUNT = 'ADD_COUNT'
-export const SET_LOADING = "SET_LOADING"
-export const SET_ERROR = 'SET_ERROR'
-export const GET_STATE = 'GET_STATE'
